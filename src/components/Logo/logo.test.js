03fc16181c9c2c359770ba2c590973a63e4c4327
washingtondeveloper <washingtonprogramador@gmail.com>
2019-10-10 13:32:34 -0300

    testando component Logo

---
+++ src/components/Logo/logo.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Logo from "./";
+
+describe("COmponent", () => {
+  let wrapper;
+  beforeAll(() => {
+    wrapper = shallow(<Logo />);
+  });
+
+  test("Deve renderizar sem erros", () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  test("Deve ter uma imagem com o nome git.png", () => {
+    expect(wrapper.find("img").length).toBe(1);
+    expect(wrapper.find("img").prop("src")).toEqual("git.png");
+  });
+
+  test("Deve ter um H1 com titulo GitUsers", () => {
+    expect(wrapper.find("h1").length).toBe(1);
+    expect(wrapper.find("h1").text("GitUsers")).toEqual("GitUsers");
+  });
+});
